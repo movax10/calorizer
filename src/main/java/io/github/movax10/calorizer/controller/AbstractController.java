6350790e90c1bfce2dda272ba8d8e061d7758889
FixError <FixError@169.254.219.56>
2015-08-21 00:18:28 +0300

    Create: AbstractController, FacebookController, config.properties, log4j.dtd, log4j.xml Update: LoginController, pom.xml

---
+++ src/main/java/io/github/movax10/calorizer/controller/AbstractController.java
@@ -0,0 +1,11 @@
+package io.github.movax10.calorizer.controller;
+
+import org.apache.log4j.Logger;
+/**
+ * @author radchenko.as
+ * @version 1.0
+ */
+public abstract class AbstractController {
+	protected final Logger LOGGER = Logger.getLogger(getClass());
+	
+}
